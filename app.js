a56f7fc9372f46402d9d89876b5dc9f4e87ff792
MaatheusRaposo <matheus.compta@gmail.com>
2026-05-27 14:40:38 -0300

    Terceiro exercicio, interatividade com JavaScript

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark_theme')
+
+    var className = document.body.className;
+    if (className == 'light_theme'){
+        this.textContent = "Dark";
+
+    }else{
+        this.textContent = "Light";
+    }
+
+    console.log('Current className: ' + className);
+    
+});
